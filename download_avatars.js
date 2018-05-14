9687206ed87d1c72e618c7de9476c520ea953cd6
JacobMJones <theretherehuman@gmail.com>
2018-05-14 22:19:44 +0000

    adds token

--- download_avatars.js
+++ download_avatars.js
@@ -1,2 +1,22 @@
 var request = require('request');
+var secrets = require('secrets');
+
 console.log('Jacob Presents: The GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request'
+    }
+  };
+
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
